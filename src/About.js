7b46c0c66c6f8c7da6f8163563c2de73f38601c3
Mason <adams.mason25@gmail.com>
2023-04-12 21:38:09 -0500

    Added temp pages

--- src/About.js
+++ src/About.js
@@ -1,8 +1,33 @@
+import React from 'react';
+import {
+  Box,
+  Text,
+  Flex,
+  Stack
+} from '@chakra-ui/react';
+
+
 function About() {
     return (
-      <div>
-        <h2>About Me</h2>
-      </div>
+      <Box
+      w='100%'
+      h='100vh'
+      bgGradient={[
+        'linear(to-tl, #2CD383, #0648F9)',
+        'linear(to-br, #0648F9, #2CD383)',
+      ]}
+    >
+        <Flex justify='center'>
+          <Stack spacing='0px'>
+            <Text color='white' fontSize={['3xl', '4xl', '5xl','6xl']} fontWeight='extrabold'>
+                Under construction :(
+            </Text>
+            <Text color='white' fontSize={['3xl', '4xl', '5xl','6xl']} fontWeight='extrabold'>
+            </Text>
+          </Stack>
+        </Flex>
+      </Box>      
+      
     );
   }
 
--- src/Projects.js
+++ src/Projects.js
@@ -1,8 +1,33 @@
+import React from 'react';
+import {
+  Box,
+  Text,
+  Flex,
+  Stack
+} from '@chakra-ui/react';
+
+
 function Projects() {
     return (
-      <div>
-        <h2>Projects</h2>
-      </div>
+      <Box
+      w='100%'
+      h='100vh'
+      bgGradient={[
+        'linear(to-tl, #2CD383, #0648F9)',
+        'linear(to-tr, #0648F9, #2CD383)',
+      ]}
+    >
+        <Flex justify='center'>
+          <Stack spacing='0px'>
+            <Text color='white' fontSize={['3xl', '4xl', '5xl','6xl']} fontWeight='extrabold'>
+                Under construction :(
+            </Text>
+            <Text color='white' fontSize={['3xl', '4xl', '5xl','6xl']} fontWeight='extrabold'>
+            </Text>
+          </Stack>
+        </Flex>
+      </Box>      
+      
     );
   }
 
